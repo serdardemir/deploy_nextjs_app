ac2e38afb157e4bd81727f451bae2d282776996a
Serdar Demir <serdardemir@outlook.com>
2024-05-31 15:07:04 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
           <h2 className={inter.className}>
             5minslearn <span>-&gt;</span>
           </h2>
-          <p className={inter.className}>Learn tech in 5mins</p>
+          <p className={inter.className}>Cloud Apps</p>
         </a>
 
         
